52a78dbcd43fa3a484ae1e79f90db5ce6d5267ab
davco <davfr_cohen@msn.com>
2018-01-28 13:07:34 +0200

    Create server folder

---
+++ server/server.py
@@ -0,0 +1,20 @@
+import os
+
+SERVER_PORT = 8080
+WEB_SERVICE_PORT = 7979
+
+def add_user(id):
+    
+
+def main():
+    """ Server of IsMyTweet << Should be running on the same host than the server """
+    """ 1) Wait for user input to update database << Open socket for Flask app 
+        2) Check for each user if change in nb of twitter 
+        3) If change, run checker twitter << use deep learning / nlp fct on message scrapped 
+        TODO : 
+            - message scrapped saved in DB ?
+    """
+
+
+if __name__ == "__main__":
+    main()
